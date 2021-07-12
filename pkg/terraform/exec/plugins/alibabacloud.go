edf857a36799972f98801c3c1b7267c6db2dfa0f
sunhui <wb-sh373163@alibaba-inc.com>
2021-07-13 00:20:23 +0800

    fix 'platorm' to 'platform' and update platformcredscheck

--- pkg/terraform/exec/plugins/alibabacloud.go
+++ pkg/terraform/exec/plugins/alibabacloud.go
@@ -9,6 +9,7 @@ func init() {
 	// "github.com/terraform-providers/terraform-provider-alicloud/alicloud"
 	exec := func() {
 		plugin.Serve(&plugin.ServeOpts{
+			// TODO AlibabaCloud: There is a multi-version dependency problem with k8s.io/client-go v11.0.0+incompatible, future support.
 			// ProviderFunc: alicloud.Provider,
 		})
 	}
