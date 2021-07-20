d2db1beb638d10e5e88eeebee9a070d91d18f71f
sunhui <wb-sh373163@alibaba-inc.com>
2021-07-20 11:20:55 +0800

    update RAM role policy

--- data/data/alibabacloud/ram/outputs.tf
+++ data/data/alibabacloud/ram/outputs.tf
@@ -5,3 +5,11 @@ output "role_master_name" {
 output "role_master_arn" {
   value = alicloud_ram_role.role_master.arn
 }
+
+output "role_worker_name" {
+  value = alicloud_ram_role.role_worker.name
+}
+
+output "role_worker_arn" {
+  value = alicloud_ram_role.role_worker.arn
+}
