17e9f39c987871fe9473a1b574001fc41eceab72
sunhui <wb-sh373163@alibaba-inc.com>
2021-08-27 12:29:15 +0800

    Alibaba: add snat to the master node VSwitchs
    The masters require internet access so the instances can pull images during startup, add snat to the master node VSwitchs.
    
    Signed-off-by: sunhui <wb-sh373163@alibaba-inc.com>

--- data/data/alibabacloud/cluster/vpc/nat_gateway.tf
+++ data/data/alibabacloud/cluster/vpc/nat_gateway.tf
@@ -14,9 +14,11 @@ resource "alicloud_nat_gateway" "nat_gateway" {
   )
 }
 
-resource "alicloud_snat_entry" "snat_entry" {
+resource "alicloud_snat_entry" "snat_entrys" {
+  count = length(var.zone_ids)
+
   depends_on        = [alicloud_eip_association.eip_association]
   snat_table_id     = alicloud_nat_gateway.nat_gateway.snat_table_ids
-  source_vswitch_id = alicloud_vswitch.vswitch_nat_gatway.id
+  source_vswitch_id = alicloud_vswitch.vswitchs[count.index].id
   snat_ip           = alicloud_eip_address.eip.ip_address
 }
